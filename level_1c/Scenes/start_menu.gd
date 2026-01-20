a925d361462bca92a4f77315a9883ce43400fb34
emilthel <emil.thelberg@icloud.com>
2026-01-20 16:02:26 +0100

    completed title screen

--- level_1c/Scenes/start_menu.gd
+++ level_1c/Scenes/start_menu.gd
@@ -14,11 +14,15 @@ func _ready() -> void:
 	score_counter.text = score
 
 func update_score(score_num): 
-	var score_file = FileAccess.open("res://Score.txt", FileAccess.WRITE)
-	var score = str(score_num)
-	score_file.store_string(score)
-	score_file.close()
+	var score_file = FileAccess.open("res://Score.txt", FileAccess.READ_WRITE)
 	
+	"Updates if score is higher"
+	var previous_score_num = int(score_file.get_as_text())
+	if score_num > previous_score_num: 
+		var score = str(score_num)
+		score_file.store_string(score)
+		score_file.close()
+		
 func start():
 	print("start")
 	"Unloads self"
@@ -28,6 +32,8 @@ func start():
 	"Loads game engine"
 	level.process_mode = Node.PROCESS_MODE_INHERIT
 	level.visible = true
+	
+	Player.screen_counter.visible = true
 	
 func _process(delta: float) -> void:
 	if Input.is_anything_pressed(): #Press any key to start
--- level_1c/level.gd
+++ level_1c/level.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
-@export var start_screen = -1
+@export var start_screen = 0
 
 @onready var screen_index = start_screen
 enum{SCREEN, TRANSITION}
 var mode = TRANSITION
-var tutorial_transition_scene = "res://level_1c/Scenes/tutorial_transition.tscn"
 var transition_scene = "res://level_1c/Scenes/transition.tscn"
 var screen
 var new_screen
@@ -17,7 +16,6 @@ var transition
 func _ready() -> void:
 	transition = load(transition_scene).instantiate()	#Loads tutorial
 	add_child(transition)
-	transition.screen_counter.text = str(screen_index + 1) #Displays number of upcoming screen
 	var screen_name = "Screen" + str(screen_index)
 	screen = get_node(screen_name)
 	
@@ -36,8 +34,6 @@ func enter_transition():
 	add_child(transition)
 	mode = TRANSITION
 
-	#Displays screen
-	transition.screen_counter.text = str(screen_index + 1) #Displays number of upcoming screen
 	return transition
 
 func get_next_screen():
@@ -81,6 +77,9 @@ func next_screen():
 		else:
 			print("Screen transition failed")
 			
-			
+	
+func restart():
+	get_tree().reload_current_scene()
+	
 func _process(delta: float) -> void:
 	pass
