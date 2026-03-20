1a6929a3070b478c3d6c3df052fa037c805ece71
emilthel <emil.thelberg@icloud.com>
2026-03-20 12:33:18 +0100

    organized player script

--- level_1c/Scenes/player.gd
+++ level_1c/Scenes/player.gd
@@ -1,12 +1,10 @@
 extends Area2D
-var invincible = false
-
+#region Variables
 @export var enemy_layer: int = 2
 @export var goal_layer: int = 4
 @export var max_blood: float = 2
 @export var start_lives: int = 2
 @export var slowmo_time: float = 3
-
 @onready var blood = max_blood
 @onready var test = max_blood
 @onready var invincibility: float = 0
@@ -31,20 +29,18 @@ var invincible = false
 @onready var visibility_notifier = $VisibilityNotifier
 @onready var next_up_screen = $GUI/NextUpScreen
 @onready var next_up_screen_name = $GUI/NextUpScreen/ScreenName
-
-#@onready var goals_collected = 0
 @onready var score = "res://Score.txt"
-var level
-var start_menu
+var invincible = false
 var progress_slots_filled = 0
 var progress_slots: Dictionary = {}
-var lives
-
 enum{GAME_OVER_RECOVERY}
-var state
-
 enum{RED,GREEN}
 var flash_color = RED
+var level
+var state
+var lives
+var start_menu
+#endregion
 
 func _ready() -> void:
 	lives = start_lives
@@ -55,13 +51,6 @@ func _ready() -> void:
 	slowmo_activate()
 	_progress_checklist_length(0)
 	slow_mo.slowmo_time = slowmo_time
-	
-func _input(event: InputEvent) -> void:
-	#if event is InputEventMouseMotion:
-		#return
-	pass
-		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	"Movement"
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN) 
@@ -145,7 +134,7 @@ func _process(delta: float) -> void:
 		
 	#print(slow_mo.process_mode)
 
-
+#region Progress checklist
 "Changes length of checklist"
 func _progress_checklist_length(length):
 	"Clears progress checklist"
@@ -159,8 +148,6 @@ func _progress_checklist_length(length):
 		slot.position.x = 100*n
 		progress_checklist.add_child(slot)
 		progress_slots[n] = slot
-
-
 "Changes displayed score"
 func _progress_checklist_score(count):
 	for n in range(len(progress_slots)):
@@ -179,109 +166,10 @@ func _progress_checklist_score(count):
 	
 #func _on_invincibility_timer_timeout() -> void:
 	#invincible = false
+#endregion
 
-func _die():
-	print("die")
-	lives -= 1
-	if lives == 0: #Game over
-		game_over()
-	else:
-		level.die()				
-		blood = max_blood
-		
-		death_sound.play()
-		death_screen.visible = true 
-		
-		_progress_checklist_score(0)
-		
-		level.transition.color_rect.color = Color(1,0,0,0.3)
-func _hit(damage):	
-	if invincibility == 0:
-		blood -= damage 
-		invincibility = 0.4
-		hit_sound.play()
-	if blood <= 0: #Death
-		_die()
-	
-func game_over():
-	print("game_over")
-	level.restart()
-
-	invincibility = 5 #Red flash
-	game_over_sound.play() #Plays sound
-	game_over_screen.visible = true 
-	
-	meaning_corrupted_music.stop() #Stops music
-	
-	"Restarts /resets player values"
-	lives = start_lives
-	blood = max_blood
-	lives_counter.visible = false
-	_progress_checklist_length(0)
-	next_up_text.visible = false
-	screen_name.visible = false
-	slow_mo.reset_slowmo()
-	#get_tree().reload_current_scene()
-			
-	
-func on_transition_entered(update_name_indicator = true):
-	if update_name_indicator:
-		print("on_transition_entered")
-		var new_screen = level.get_next_screen()
-		if new_screen:
-			progress_bar_cover.scale.x = 0.3 * (new_screen.goals_needed -1)
-			screen_name.text = new_screen.screen_name
-			next_up_screen_name.text = new_screen.screen_name
-			next_up_screen.visible = true
-			print(new_screen)
-			print(new_screen.screen_name)
-
-
-	"Initializes screen name,for when entering level 1"
-	screen_name.visible = true
-
-	#goals_collected = 0
-	invincibility = 0.3
-	
-	slow_mo._enter_cooldown_state()
-	slow_mo.reset_slowmo()
-
-
-func on_screen_0_entered():
-	print("on_screen_0_entered")
-	lives_counter.visible = true
-	
-	_progress_checklist_length(level.screen.goals_needed)
-
-	
-func on_screen_entered():
-	print("on_screen_entered")
-	"Changes invincibility color to red"
-	bg.modulate = Color(1,0,0,0)
-	
-	"Removes 'next up' text"
-	next_up_text.visible = false
-	_progress_checklist_length(level.screen.goals_needed)
-	
-	
-func on_start_menu_exited():
-	game_over_screen.visible = false #Hides game over text
-	invincibility = 0 #Stops red flash
-	game_over_sound.stop() #Stops game over sound
-	meaning_corrupted_music.play() #Starts music
-
-#func on_level_entered():
-	#print("on_level_entered")
-	#var new_screen = level.get_next_screen()
-	#progress_bar_cover.scale.x = 0.3 * (new_screen.goals_needed -1)
-	#_progress_checklist_length(new_screen.goals_needed)
-	#goals_collected = 0
-	#invincibility = 1
-	#"Changes invincibility color to green"
-	#bg.modulate = Color(0,1,0,0)
-	
-	
-"On hitting enemies or collecting goals"
+#region Collision
+"Collision signal"
 func _on_area_entered(area: Area2D) -> void: #Collision
 	#print("on_area_entered")
 	if area.collision_layer == enemy_layer and invincibility == 0: #Hit
@@ -309,11 +197,91 @@ func _on_area_entered(area: Area2D) -> void: #Collision
 			_progress_checklist_score(level.screen.goals_collected)
 			
 	slowmo_activate()
+func _die():
+	print("die")
+	lives -= 1
+	if lives == 0: #Game over
+		game_over()
+	else:
+		level.die()				
+		blood = max_blood
+		
+		death_sound.play()
+		death_screen.visible = true 
+		
+		_progress_checklist_score(0)
+		
+		level.transition.color_rect.color = Color(1,0,0,0.3)
+func _hit(damage):	
+	if invincibility == 0:
+		blood -= damage 
+		invincibility = 0.4
+		hit_sound.play()
+	if blood <= 0: #Death
+		_die()
+func game_over():
+#endregion
+	print("game_over")
+	level.restart()
+
+	invincibility = 5 #Red flash
+	game_over_sound.play() #Plays sound
+	game_over_screen.visible = true 
+	
+	meaning_corrupted_music.stop() #Stops music
+	
+	"Restarts /resets player values"
+	lives = start_lives
+	blood = max_blood
+	lives_counter.visible = false
+	_progress_checklist_length(0)
+	next_up_text.visible = false
+	screen_name.visible = false
+	slow_mo.reset_slowmo()
+	#get_tree().reload_current_scene()
+			
+#region Level transitions
+func on_transition_entered(update_name_indicator = true):
+	if update_name_indicator:
+		print("on_transition_entered")
+		var new_screen = level.get_next_screen()
+		if new_screen:
+			progress_bar_cover.scale.x = 0.3 * (new_screen.goals_needed -1)
+			screen_name.text = new_screen.screen_name
+			next_up_screen_name.text = new_screen.screen_name
+			next_up_screen.visible = true
+			print(new_screen)
+			print(new_screen.screen_name)
+
+
+	"Initializes screen name,for when entering level 1"
+	screen_name.visible = true
+
+	#goals_collected = 0
+	invincibility = 0.3
+	
+	slow_mo._enter_cooldown_state()
+	slow_mo.reset_slowmo()
+func on_screen_0_entered():
+	print("on_screen_0_entered")
+	lives_counter.visible = true
+	
+	_progress_checklist_length(level.screen.goals_needed)	
+func on_screen_entered():
+	print("on_screen_entered")
+	"Changes invincibility color to red"
+	bg.modulate = Color(1,0,0,0)
+	
+	"Removes 'next up' text"
+	next_up_text.visible = false
+	_progress_checklist_length(level.screen.goals_needed)
+func on_start_menu_exited():
+	game_over_screen.visible = false #Hides game over text
+	invincibility = 0 #Stops red flash
+	game_over_sound.stop() #Stops game over sound
+	meaning_corrupted_music.play() #Starts music
+#endregion
 
 func slowmo_activate():
 	slow_mo.process_mode = Node.PROCESS_MODE_INHERIT
 	slow_mo.visible = true
-
-func force_onscreen(pos: Vector2):
-	pass
-		
