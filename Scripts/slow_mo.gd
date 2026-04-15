877b1d504a7171d0eaa3f9ad1ad2b0c8616f97d3
emilthel <emil.thelberg@icloud.com>
2026-04-15 16:42:43 +0200

    commented slowmo, cleaned up by putting ability bar cover as child of BG instead of special node referenced in code

--- Scripts/slow_mo.gd
+++ Scripts/slow_mo.gd
@@ -1,59 +1,69 @@
 extends Area2D
 
 var time_speed: float = 1
-enum{COOLDOWN, SLOWMO}
+enum{COOLDOWN, READY, SLOWMO}
 var state = COOLDOWN
 var slowmo_time: float = 1
 var slowmo_cooldown: float = 2 
 
 var is_frame1: bool = true
 var slowmo_time_left: float
-var slowmo_cooldown_left: float
+var slowmo_cooldown_left: float = 0
 var level
 @onready var slowmo_bg = $SlowmoBG
 @onready var ability_bar = $AbilityBar
-@onready var ability_bar_cover = $AbilityBarCover
 
 func _ready() -> void:
 	slowmo_time_left = slowmo_time
-	slowmo_cooldown_left = 0
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	"Sets global time speed"
+	"Makes time speed global"
 	TimeManager.global_time_speed = time_speed
 	
 	match state:
-		COOLDOWN:
-			time_speed = 1
-			slowmo_cooldown_left -= delta
-			slowmo_bg.modulate.a *= 0.00000001**delta #
-			ability_bar_cover.modulate.a = slowmo_bg.modulate.a
-			ability_bar.scale.x = 1 - slowmo_cooldown_left/slowmo_cooldown
+		COOLDOWN: #Charging up slowmo
+			"If time runs out, now ready"
+			slowmo_cooldown_left -= delta #Cooldown
 			if slowmo_cooldown_left < 0:
-				if Input.is_action_just_pressed("slowmo"):
-					_enter_slowmo_state()
+				_enter_ready_state()
 			
-		SLOWMO:
+			"Shows cooldown with ability bar"
+			ability_bar.scale.x = 1 - slowmo_cooldown_left/slowmo_cooldown  #Depletes ability bar
+			"Fades out background"
+			slowmo_bg.modulate.a *= 0.00000001**delta #Fades out background
+
+				
+		READY: #Ready to use slowmo
+			if Input.is_action_just_pressed("slowmo"):
+				_enter_slowmo_state()
+		
+		SLOWMO: #Using slowmo
+			"Slows down time"
 			time_speed = 0.2
+			
+			"If releasing space or if time runs out, exits slowmo"
 			slowmo_time_left -= delta
+			if Input.is_action_just_released("slowmo") or slowmo_time_left <0: 
+				time_speed = 1 #Resets time speed
+				enter_cooldown_state()
+		
+			"Shows time left with background color"
 			slowmo_bg.modulate.a = (slowmo_time - slowmo_time_left)/slowmo_time
-			ability_bar_cover.modulate.a = slowmo_bg.modulate.a
-			if Input.is_action_just_released("slowmo"):
-				_enter_cooldown_state()
-			if slowmo_time_left < 0:
-				_enter_cooldown_state()
 
 
-#State change functions
+
+#########STATE CHANGE
 func _enter_slowmo_state():
 	slowmo_time_left = slowmo_time
 	state = SLOWMO
-
 	
-func _enter_cooldown_state():
-	state = COOLDOWN
-	slowmo_cooldown_left = slowmo_cooldown
+func _enter_ready_state():
+	state = READY
 
+func enter_cooldown_state():
+	state = COOLDOWN
+	slowmo_cooldown_left = slowmo_cooldown #Restarts cooldown
+	
 func reset_slowmo():
 	slowmo_cooldown_left = 0
